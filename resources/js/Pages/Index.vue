94cddaa28340fe9772b2ad6776d7f61990bd57c3
ggz2468 <guiguzi19971006@gmail.com>
2026-04-11 01:58:14 +0800

    新增首頁

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,22 @@
+<script setup>
+import NavBar from '@/components/ui/NavBar.vue';
+</script>
+
+<style scoped>
+nav {
+    background-color: #000000;
+    color: #ffffff;
+    padding: 1rem;
+    position: fixed;
+    top: 0;
+    width: 100%;
+}
+
+nav h1 {
+    font-family: '新細明體', serif;
+}
+</style>
+
+<template>
+    <NavBar />
+</template>
