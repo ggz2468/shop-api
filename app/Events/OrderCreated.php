d758463ea943f5146074db2bd11015e61e8d8350
ggz2468 <guiguzi19971006@gmail.com>
2026-09-22 02:54:36 +0800

    refactor(events): 移除 OrderCreated 未使用的廣播樣板

--- app/Events/OrderCreated.php
+++ app/Events/OrderCreated.php
@@ -2,33 +2,17 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class OrderCreated
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
     /**
-     * Create a new event instance.
-     *
      * @return void
      */
     public function __construct(
         public int $orderId,
     ) {}
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
-    }
 }
